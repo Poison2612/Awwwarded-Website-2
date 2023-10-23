c614b9a9d9f98c81017ad90a0e087c1eb6a6516a
Sahil Solankey <poison@Sahils-MacBook-Air.local>
2023-10-23 13:11:41 +0530

    Caursel Left

---
+++ script.js
@@ -0,0 +1,355 @@
+//images and color and text Properties
+
+var obj = {
+    img1: [
+        'https://images.prismic.io/porte-folio/d7553a8b-5d97-4334-9de7-e0821fd55015_white.png?auto=compress,format&rect=0,200,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/d7553a8b-5d97-4334-9de7-e0821fd55015_white.png?auto=compress,format&rect=0,200,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/d7553a8b-5d97-4334-9de7-e0821fd55015_white.png?auto=compress,format&rect=0,200,1600,800&w=1600&h=800'
+    ],
+    img2: [
+        'https://images.prismic.io/porte-folio/ed57625c-d904-4ef9-9158-37b83921db2b_Armen1.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/b8a0e2e3-44c5-4c1b-87e1-9e684b58866e_Armen2.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/42822cb3-32b2-4bfb-98ca-75faf84dea13_Armen3.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/7c7d21ee-eb0a-4a02-853b-2cf6dc8f3357_Armen4.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800'
+    ],
+    img3: [
+        'https://images.prismic.io/porte-folio/4654bce7-cab7-4b66-9ea0-06063e7276d1_happy1.png?auto=compress,format&rect=16,0,1786,893&w=1600&h=800'
+    ],
+    img4: [
+        'https://images.prismic.io/porte-folio/a37963ee-ac62-47c5-95fc-80fe88734c07_SKORAN1.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/e2a3e14d-f44b-4dfe-86b2-c77a75b80b4f_SKORAN2.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/6d07b810-a7d7-4cc7-9ba1-018f803760ea_SKORAN3.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/76baf4ef-6ba2-4e99-a3d9-3a8682ed1984_SKORAN4.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800'
+    ],
+    img5: [
+        'https://images.prismic.io/porte-folio/880e4afb-82d8-433c-bc07-51cd6f93f41b_specimen-mckup-angle.png?auto=compress,format&rect=0,93,1468,734&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/6bc27d97-f658-4486-a2f2-5e60c1482f94_mckup-specimen.png?auto=compress,format&rect=0,214,1649,825&w=1600&h=800'
+    ],
+    img6: [
+        'https://images.prismic.io/porte-folio/e718143c-33b7-46b9-bee4-f1f058a88a32_Saut-5.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/c791da23-861c-4c39-9ba5-19002089e8cb_Saut.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/22760a51-bea4-4c77-a3f9-6b0f97d20a2c_Saut-1.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/4dc16a0e-ed54-4cc1-bceb-c0aed6d15810_Saut-2.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/b9585e66-11c1-4f2a-9993-314e09142b02_Saut-3.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/7050dd7b-ae14-4805-a039-6bed3bbdf2c5_Saut-4.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800'
+],
+    img7: [
+        'https://images.prismic.io/porte-folio/ba0648ec-d5dc-4f47-acc8-8e950a301207_feel1.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/482bc567-339f-48e7-9af9-ee52558684a8_feel2.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/d22397b9-2c3a-40dc-965d-cb9d2e6392f4_feel3.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/4414c888-d597-4840-9e0e-902814823325_feel5.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800',
+        'https://images.prismic.io/porte-folio/0524509e-211e-431c-9c1b-235e0a834eb0_feel4.png?auto=compress,format&rect=0,0,1600,800&w=1600&h=800'
+    ],
+    img8: [
+        'https://images.prismic.io/porte-folio/4e0d3c61-1a98-4b82-99db-76854c949b36_moke+up+typo+modulaire.jpg?auto=compress,format&rect=0,224,1649,825&w=1600&h=800'
+    ]
+};
+
+var data = [
+    {
+        "data-bg-color": "#000000ff", 
+        "data-fill-color": "#fbf9f9ff", 
+        "data-text-up": "dault", 
+        "data-text-down": "lafon"
+    },
+    {
+        "data-bg-color": "#f6f6f6ff",  
+        "data-fill-color": "#040406ff", 
+        "data-text-up": "armen",
+        "data-text-down": "sante"
+    },
+    {
+        "data-bg-color": "#581a0bff",  
+        "data-fill-color": "#f3c45eff", 
+        "data-text-up": "happy", 
+        "data-text-down": "hour"
+    }, 
+    {
+        "data-bg-color": "#950e13ff",  
+        "data-fill-color": "#f3c45eff", 
+        "data-text-up": "Skoran", 
+        "data-text-down": "Delta"
+    },
+    {
+        "data-bg-color": "#192042ff",  
+        "data-fill-color": "#d4b639ff", 
+        "data-text-up": "Spicmen", 
+        "data-text-down": "didot"
+    },
+    {
+        "data-bg-color": "#010101ff",  
+        "data-fill-color": "#ed6f24ff", 
+        "data-text-up": "saut", 
+        "data-text-down": "hermes"
+    },
+    {
+        "data-bg-color": "#5a5780ff",  
+        "data-fill-color": "#e17134ff", 
+        "data-text-up": "Feel the", 
+        "data-text-down": "texture"
+    },
+    {
+        "data-bg-color": "#dab3a1ff",  
+        "data-fill-color": "#142954ff", 
+        "data-text-up": "modular",
+        "data-text-down": "design"
+    }
+];
+
+function makeImageryDivs() {
+const section = document.querySelector("#section");
+
+for (j = 0; j < data.length; j++) {
+const obj = data[j];
+const div = document.createElement("div");
+div.setAttribute("id", "img" + (j + 1));
+div.setAttribute("class", "imagery");
+
+for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+    const value = obj[key];
+    div.setAttribute(key, value);
+    }
+}
+
+section.appendChild(div);
+}
+// Loop through each property of the obj object
+for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+    const div = document.querySelector(`#${key}`);
+    const imgUrls = obj[key];
+
+      // Loop through each image URL in the property's array
+    for (const url of imgUrls) {
+        const img = document.createElement("img");
+        img.src = url;
+        div.appendChild(img);
+    }
+    }
+}
+}
+makeImageryDivs();
+
+
+
+function revealToSpan(){
+    document.querySelectorAll(".reveal")
+.forEach(function(elem){
+    //create two spans
+    var parent = document.createElement("span");
+    var child = document.createElement("span");
+
+    // parent and child both sets their respective classes
+    child.classList.add("child");
+    parent.classList.add("parent"); 
+
+    //span parent gets child and child gets elem details
+    child.innerHTML = elem.innerHTML;
+    parent.appendChild(child);
+
+    //elem replace its value with parent span
+    elem.innerHTML = "";
+    elem.appendChild(parent);
+    }) 
+}
+revealToSpan();
+
+function loaderAnimation() {
+    var tl = gsap.timeline();
+
+tl
+.to('#loader h3', {
+    opacity: 0,
+    duration: 0.2,
+    ease: 'expo.inOut',
+    delay: 1.8
+})
+.to('#loader', {
+    y: '-100%',
+    zIndex: 0,
+    onComplete: ticketFeature,
+})
+.from('#section', {
+    width: '0',
+    duration: 1,
+    ease: 'expo.inOut',
+    stagger: 0.1
+})
+.to('#logo', {
+    opacity: 1,
+    duration: 5,
+    ease: 'expo.inOut',
+})
+}
+loaderAnimation();
+
+var isImageryFullWidth = false;
+
+function ticketFeature() {
+
+document.querySelectorAll('.imagery').forEach(button => {
+    button.addEventListener('click', () => {
+
+        isImageryFullWidth = true;
+
+        const bgColor = button.getAttribute('data-bg-color');
+        document.querySelector("#main").style.backgroundColor = bgColor;
+
+        const fillColor = button.getAttribute('data-fill-color');
+        document.querySelectorAll(".mid").forEach(button => {
+            button.style.backgroundColor = fillColor;
+            button.style.color = bgColor;
+        });
+        document.querySelectorAll('#cross-holder > div > span').forEach(button => {
+            button.style.backgroundColor = fillColor;
+        });
+        document.querySelectorAll("h6").forEach(button => {
+            button.style.color = fillColor;
+        }); 
+        document.querySelectorAll(".logoHeader path").forEach(button => {
+            button.style.fill = fillColor;
+        });
+
+        const textUp = button.getAttribute('data-text-up');
+        const textDown = button.getAttribute('data-text-down');
+
+        document.querySelectorAll("#mid-nav h1").forEach(button => {
+            button.textContent = textUp;
+        });
+
+        document.querySelector("#client .parent .child").textContent = textUp + " " + textDown;
+
+        document.querySelectorAll("#mid-foot h1").forEach(button => {
+            button.textContent = textDown;
+        });
+        gsap.to('.imagery', {
+            filter: 'saturate(0.1) brightness(0.5) opacity(0)',
+            width: '0',
+        });
+        gsap.to(button, {
+            filter: 'saturate(1) brightness(1) opacity(1)',
+            width: '100%',
+        });
+        gsap.to('.mid',{
+            height: '8vw',
+            duration: 0.5,
+            ease: 'expo.inOut',
+        })
+        gsap.to('.reveal .parent .child', {
+            transform: 'translateY(0%)',
+            duration: 0.5,
+            ease: 'expo.inOut',
+        })
+        gsap.to('.lft',{
+            width: '40%',
+            duration: 1,
+            transition: 'all 0.3s expo.Out'
+        })
+        gsap.to('.mdl',{
+            height: '90%',
+            duration: 1,
+            transition: 'all 0.3s expo.Out'
+        })
+        gsap.to('.rgt',{
+            width: '40%',
+            duration: 1,
+            transition: 'all 0.3s expo.Out'
+        })
+        
+    })
+});
+
+document.body.addEventListener('click', (event) => {
+    const section = event.target.closest('#section , #cross-holder > div');
+
+    if (!section) {
+        isImageryFullWidth = false;
+        gsap.to('.imagery', {
+            filter: 'saturate(0.1) brightness(0.5) opacity(1)',
+            width: '12.25%',
+        });
+        gsap.to('.mid',{
+            height: "0",
+            duration: 0.5,
+            ease: 'expo.inOut',
+        })
+        gsap.to('.reveal .parent .child', {
+            transform: 'translateY(100%)',
+            duration: 0.5,
+            ease: 'expo.inOut',
+        })
+        gsap.to('.lft',{
+            width: '0%',
+            duration: 1,
+            transition: 'all 0.3s expo.Out'
+        })
+        gsap.to('.mdl',{
+            height: '0%',
+            duration: 1,
+            transition: 'all 0.3s expo.Out'
+        })
+        gsap.to('.rgt',{
+            width: '0%',
+            duration: 1,
+            transition: 'all 0.3s expo.Out'
+        })
+        document.querySelector("#main").style.backgroundColor = ''; 
+
+        document.querySelectorAll(".mid").forEach(button => {
+            button.style.backgroundColor = '';
+            button.style.color = '';
+        });
+
+        document.querySelectorAll("h6").forEach(button => {
+            button.style.color = '';
+        });
+
+        document.querySelectorAll(".logoHeader path").forEach(button => {
+            button.style.fill = '';
+        });
+    }
+});
+}
+
+function hoverAnimation() {
+        document.querySelectorAll('.imagery').forEach(button => {
+            button.addEventListener('mouseenter', () => {
+                if(!isImageryFullWidth) {
+                    button.style.filter = 'saturate(1) brightness(1) opacity(1)';
+                }
+            });
+            button.addEventListener('mouseleave', () => {
+                if(!isImageryFullWidth) {
+                    button.style.filter = 'saturate(0.1) brightness(0.5) opacity(1)';
+                }
+            });
+    });
+}
+hoverAnimation();
+
+function crossAnimation(){
+    var nRotatingAngle = 0;
+
+document.querySelector('#cross-left').addEventListener('click', function() {  
+    nRotatingAngle -= 90;
+    gsap.to('#cross-left', {
+        rotation: nRotatingAngle + 'deg',
+        duration: 0.3,
+    })
+    console.log(nRotatingAngle);
+});
+var pRotatingAngle = 0;
+
+document.querySelector('#cross-right').addEventListener('click', function() {  
+    pRotatingAngle += 90;
+    gsap.to('#cross-right', {
+        rotation: pRotatingAngle + 'deg',
+        duration: 0.3,
+    })
+    console.log(pRotatingAngle);
+});
+}
+
+crossAnimation();
+
